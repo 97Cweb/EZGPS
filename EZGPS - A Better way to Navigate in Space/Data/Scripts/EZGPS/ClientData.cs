84d16d8a83720ca25b3a45f0d254fb9707911752
97Cweb <63621179+97Cweb@users.noreply.github.com>
2024-05-21 18:03:02 -0400

    Add files via upload

---
+++ EZGPS - A Better way to Navigate in Space/Data/Scripts/EZGPS/ClientData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ProtoBuf;
+
+
+namespace EZGPS
+{
+
+    [ProtoContract]
+    public class ClientData
+    {
+        [ProtoMember(1)]
+        public long playerID;
+
+        [ProtoMember(2)]
+        public String text;
+
+        public ClientData(long playerID, String text)
+        {
+            this.playerID = playerID;
+            this.text = text;
+        }
+
+        public ClientData() { }
+
+    }
+}
