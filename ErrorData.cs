ad42745c5f5fabaec786e776de29d66f78eb6b1f
97Cweb <63621179+97Cweb@users.noreply.github.com>
2024-05-21 18:02:29 -0400

    Add files via upload

---
+++ ErrorData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ProtoBuf;
+
+
+namespace EZGPS
+{
+
+    [ProtoContract]
+    public class ErrorData
+    {
+        [ProtoMember(1)]
+        public long playerID;
+
+        [ProtoMember(2)]
+        public String message;
+
+        public ErrorData(long playerID, String message)
+        {
+            this.playerID = playerID;
+            this.message = message;
+        }
+
+        public ErrorData() { }
+
+    }
+}
